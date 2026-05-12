8dac2063b7f2f7b64e53f2d21adb56f7ba73fa68
Ofek-Hodis <ofkhod@gmail.com>
2026-05-12 16:53:34 +0200

    array exercises and bubble sort

--- C2TP1.c
+++ C2TP1.c
@@ -14,8 +14,9 @@ void afficher_tableau(int t[], const int size)
 {
     for (int i=0; i<size; i++)
     {
-        printf("%d, ", t[i]);
+        printf("%d ", t[i]);
     }
+    printf("\n");
 }
 
 int somme_tableau(int t[], const int size)
@@ -61,11 +62,85 @@ int max_tableau(int t[], const int size)
     return t[max];
 }
 
+void inverse_array(int t[], const int n)
+{
+    int temp;
+    for (int i=0; i<n/2; i++)
+    {
+        temp = t[i];
+        t[i] = t[n-1-i];
+        t[n-1-i] = temp;
+    }
+}
+
+int does_contain(int t[], const int N, const int value)
+{
+    for (int i=0; i<N; i++)
+    {
+        if (t[i] == value)
+            return 1;
+    }
+    return 0;
+}
+
+int remove_doubles(int src[], const int N, int dest[])
+{
+    int counter = 0;
+    for (int i=0; i<N; i++)
+    {
+        if (does_contain(dest, counter, src[i]))
+            continue;
+        dest[counter] = src[i];
+        counter++;
+    }
+    return counter;
+}
+
+void bubble_sort(int t[], const int N)
+{
+    int temp=0;
+    for (int i = N-1; i >=0; i--)
+    {
+        int is_changed=0;
+        for (int j=0; j<i; j++)
+        {
+            if (t[j]>t[j+1])
+            {
+                temp = t[j+1];
+                t[j+1] = t[j];
+                t[j] = temp;
+                is_changed = 1;
+            }
+        }
+        if (!is_changed)
+            return;
+    }
+}
+
 int main()
 {
-    int t[] = {4, 7, 1, 9, 3, 8, 2, 5, 6, 0};
-    printf("%d \n", somme_tableau(t, SIZE));
-    printf("%lf \n", moyenne_tableau(t, SIZE));
-    printf("%d \n", min_tableau(t,SIZE));
-    printf("%d \n", max_tableau(t,SIZE));
+    //int t[] = {4, 7, 1, 9, 3, 8, 2, 5, 6, 0};
+    //printf("%d \n", somme_tableau(t, SIZE));
+    //printf("%lf \n", moyenne_tableau(t, SIZE));
+    //printf("%d \n", min_tableau(t,SIZE));
+
+    //afficher_tableau(t, SIZE);
+    //inverse_array(t, SIZE);
+    //afficher_tableau(t, SIZE);
+
+    // Exercise 5
+    //int src[] = {1,2,2,3,4,4,4,5};
+    //const int size_src = 8;
+    //int dest[8];
+    //const int size_dest = remove_doubles(src, size_src, dest);
+    //printf("%d \n", size_dest);
+    //afficher_tableau(dest, size_dest);
+
+    // Exercise 6 testing - buble sort
+    // int t[] = {4, 7, 1, 9, 3, 8, 2, 5, 6, 0};
+    // bubble_sort(t, SIZE);
+    // afficher_tableau(t, SIZE);
+
+
+
 }
