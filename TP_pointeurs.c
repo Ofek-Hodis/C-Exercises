2b8631067f77a3d545d41f3bac0840fa5a04abd2
Ofek-Hodis <ofkhod@gmail.com>
2026-05-27 09:34:31 +0200

    Function using realloc

--- TP_pointeurs.c
+++ TP_pointeurs.c
@@ -70,6 +70,34 @@ void comparison(int *T1, const int SIZE1, int *T2, const int SIZE2)
     free(T2);
 }
 
+int extendable_array(int array[], int cap, int* new_size)
+{
+    *new_size = 0;
+    while (1)
+    {
+        printf("Enter a number, enter -1 to stop the loop\n");
+        int res;
+        scanf("%d", &res);
+        if (res == -1) break;
+        if (*new_size == cap)
+        {
+            cap = cap*2;
+            int *tmp = realloc(array, cap*sizeof(int));
+            if (tmp == NULL)
+            {
+                free (array); // Freeing memory to avoid memory leaks
+                return 1;
+            }
+            array = tmp;
+            printf("Change of capacity : %d\n", cap);
+        }
+        *new_size++;
+    }
+
+    display_array(array, *new_size);
+    return 0;
+}
+
 int main()
 {
     printf("Enter exercise number");
@@ -93,6 +121,16 @@ int main()
             int *T1 = NULL;
             int *T2 = NULL;
             comparison(T1, 5, T2, 5);
+        }
+    case 8:
+        {
+            int cap = 4;
+            int x=0;
+            int *new_size = &x;
+            int *array = malloc(sizeof(int) * cap);
+            extendable_array(array, cap, new_size);
+            display_array(array, 4);
+            free(array);
         }
     default : break;
     }
