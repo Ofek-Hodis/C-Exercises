0fd3caffe514737f79da2d67be2e1de8a59f7753
Ofek-Hodis <ofkhod@gmail.com>
2026-05-28 15:25:42 +0200

    Basic exercises handling the article structure

--- article_structure/main.c
+++ article_structure/main.c
@@ -2,6 +2,39 @@
 #include <stdlib.h>
 #include "tab-articles.h"
 
+ARTICLE* search_by_code(ARTICLE** arr, int nb, int code)
+{
+  for (int i=0; i<nb; i++)
+  {
+    if (arr[i]->code == code)
+      return arr[i];
+  }
+  return NULL;
+}
+
+double valeur_stock(ARTICLE** arr, int nb)
+{
+  double somme=0;
+  for (int i=0; i<nb; i++)
+  {
+    somme += arr[i]->prix * arr[i]->qteStock;
+  }
+  return somme;
+}
+
+ARTICLE* article_plus_cher(ARTICLE **arr, int nb)
+{
+  ARTICLE * max = *arr;
+  for (int i=0; i<nb; i++)
+  {
+    if (arr[i]->prix > max->prix)
+    {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+
 int main() {
   int n = 3;
   ARTICLE **mag;
